68ba31731311941250c155b406910eea1132a42b
Talley Lambert <talley.lambert@gmail.com>
2018-01-02 16:48:41 -0500

    change site_id for heroku

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,7 +135,7 @@ TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
-SITE_ID = 1
+SITE_ID = 2
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
--- config/settings/production.py
+++ config/settings/production.py
@@ -231,5 +231,3 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
-
-SITE_ID = 2
