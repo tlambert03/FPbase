6314da0297a8681b3631bfad1c62c02a9f0d2637
tlambert03 <talley.lambert@gmail.com>
2018-05-13 23:20:49 -0400

    misc test

--- proteins/tests/test_models.py
+++ proteins/tests/test_models.py
@@ -7,7 +7,7 @@ class TestProteinModel(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.protA = Protein.objects.create(name='ProteinA')
-        cls.protB = Protein.objects.create(name='ProteinB')
+        cls.mprotB = Protein.objects.create(name='mProteinB')
 
     def test_protein_str(self):
         self.assertEquals(str(self.protA), 'ProteinA')
@@ -16,3 +16,11 @@ class TestProteinModel(TestCase):
         # This will also fail if the urlconf is not defined.
         self.assertEquals(self.protA.get_absolute_url(),
                           '/protein/{}/'.format(self.protA.slug))
+
+    def test_mless(self):
+        # This will also fail if the urlconf is not defined.
+        self.assertEquals(self.mprotB.mless, 'ProteinB')
+
+    def test_name(self):
+        # This will also fail if the urlconf is not defined.
+        self.assertEquals(str(self.protA), 'ProteinA')
