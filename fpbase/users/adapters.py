4ea463043ced7d820f435356b0969ecb7b81bdf8
Talley Lambert <talley.lambert@gmail.com>
2018-06-16 15:52:19 -0400

    validate email MX on signup page

--- fpbase/users/adapters.py
+++ fpbase/users/adapters.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.utils.translation import ugettext_lazy as _
+from django import forms
+import dns.resolver
+import dns.exception
 
 
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
+
+    def clean_email(self, email):
+        domain = email.split('@')[-1]
+        try:
+            dns.resolver.query(domain, 'MX')
+        except dns.exception.DNSException:
+            raise forms.ValidationError(
+                _(u"The domain %s could not be found.") % domain)
+        return email
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
