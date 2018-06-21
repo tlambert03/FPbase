59f4c0cba54d1a91d0b2a9e1edc2c5edfab594ca
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-06-21 18:14:11 +0200

    Broaden reg-ex to all resolver variants

--- proteins/forms.py
+++ proteins/forms.py
@@ -5,6 +5,7 @@ from django.forms.models import inlineformset_factory  # ,BaseInlineFormSet
 from django.apps import apps
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
+import re import sub
 from proteins.models import (Protein, State, StateTransition, Spectrum,
                              ProteinCollection, BleachMeasurement)
 from proteins.validators import validate_spectrum, validate_doi, protein_sequence_validator
@@ -55,7 +56,7 @@ class DOIField(forms.CharField):
 
     def to_python(self, value):
         if value and isinstance(value, str):
-            value = value.lstrip('https://doi.org/')
+            value = value.sub('^https?://(dx\.)?doi.org/')
         return super().to_python(value)
 
 
