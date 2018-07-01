f4223c990a0fb9d109e78ab1c7aa4e157ef04c0d
Talley Lambert <talley.lambert@gmail.com>
2018-07-01 17:32:09 -0400

    remove private collection and unique_together from microscope

--- proteins/models/collection.py
+++ proteins/models/collection.py
@@ -13,20 +13,22 @@ class OwnedCollection(TimeStampedModel):
     owner = models.ForeignKey(User, blank=True, null=True,
                               related_name='%(class)s' + 's',
                               on_delete=models.SET_NULL,)
-    private = models.BooleanField(default=False, verbose_name="Private Collection",
-                                  help_text="Private collections can not be seen "
-                                            "by or shared with other users")
 
     def __str__(self):
         return self.name
 
     class Meta:
         abstract = True
-        unique_together = (('owner', 'name'),)
 
 
 class ProteinCollection(OwnedCollection):
     proteins = models.ManyToManyField('Protein', related_name='collection_memberships')
+    private = models.BooleanField(default=False, verbose_name="Private Collection",
+                                  help_text="Private collections can not be seen "
+                                            "by or shared with other users")
 
     def get_absolute_url(self):
         return reverse("proteins:collection-detail", args=[self.id])
+
+    class Meta:
+        unique_together = (('owner', 'name'),)
