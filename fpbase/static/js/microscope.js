30685f87db6f8ce1d1dd17ea54dc256629549a34
Talley Lambert <talley.lambert@gmail.com>
2018-07-16 19:58:22 -0400

    consolidate microscope page code

--- fpbase/static/js/microscope.js
+++ fpbase/static/js/microscope.js
@@ -65,6 +65,12 @@ var userOptions = {
     scaleToQY: {type: 'checkbox', msg: 'Scale emission spectra to quantum yield'},
 };
 
+var mobilecheck = function() {
+  var check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
 var chartOptions = function() {
     return {
                 focusEnable: options.focusEnable,
@@ -530,13 +536,6 @@ $(function() {
         )
     });
 
-
-    var mobilecheck = function() {
-      var check = false;
-      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-      return check;
-    };
-
     //initialize chart
     nv.addGraph(function() {
         chart = nv.models.lineChart().options(chartOptions());
@@ -596,8 +595,7 @@ $(function() {
           });
         }
         catch(error) {
-          //console.error(error);
-          console.log('could not find noUiSlider')
+          console.error(error);
         }
 
 
@@ -836,7 +834,6 @@ function updateGlobalGradient() {
 //// EFFICIENCY CALCULATIONS
 
 function spectral_product(ar1, ar2) {
-    console.log("spectral_product")
     // calculate product of two spectra.values
     var output = [];
     var step = ar1[1].x - ar1[0].x;
@@ -963,3 +960,269 @@ function efficiency(filtervalues, fluor) {
         scalar: dataitem.scalar
     }
 }
+
+
+
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() {
+        this.parentNode.appendChild(this);
+    });
+};
+
+d3.selection.prototype.moveToBack = function() {
+    return this.each(function() {
+        var firstChild = this.parentNode.firstChild;
+        if (firstChild) {
+            this.parentNode.insertBefore(this, firstChild);
+        }
+    });
+};
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
+function autoSizeText() {
+  var el, elements, _i, _len, _results;
+  elements = $('.filter-label');
+  if (elements.length < 0) {
+    return;
+  }
+  _results = [];
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+    el = elements[_i];
+    _results.push((function(el) {
+      var resizeText, _results1;
+      resizeText = function() {
+        var elNewFontSize;
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 0.5) + 'px';
+        return $(el).css('font-size', elNewFontSize);
+      };
+      _results1 = [];
+      var _c = 0;
+      while (el.scrollHeight > el.offsetHeight & (_c < 20)) {
+        _results1.push(resizeText());
+        _c += 1;
+      }
+      return _results1;
+    })(el));
+  }
+  return _results;
+};
+
+function selectedSlugs(){
+  var selected = $(".filter-selector:checked, .data-selector :selected");
+  var list;
+  if ($("#merge-light-exfilter").prop('checked') && $('#light-select :selected').val() !== '') {
+    list = selected.not('#light-select :selected, .ex-filter:checked').map(function(){ return $(this).val(); });
+    var _concat = "merged/"
+    _concat += $('#light-select :selected, .ex-filter:checked')
+                  .map(function(i, o){ return o.value})
+                  .get()
+                  .reduce(function(a,b){ return a + "/" + b })
+    if (options.normMergedEx){
+      _concat += '/normed';
+    }
+    list.push(_concat)
+  } else {
+    list = selected.map(function(){ return $(this).val(); });
+  }
+  return list.get().clean("");
+}
+
+function updateData(){
+  var selected = selectedSlugs();
+  // remove old data that isnt still in the array
+  for (var i = data.length - 1; i >= 0; i--){
+    if ($.inArray(data[i].slug, selected) == -1){
+      data.splice(i, 1);
+    };
+  };
+  // add new items
+  deferreds = [];
+  for (var n = 0; n < selected.length; n++){
+    if (selected[n] && !dataHasSlug(selected[n])){
+      if (selected[n].startsWith("laser")){
+        updateCustomLaser(selected[n].replace("laser-", ""));
+      } else if (selected[n].startsWith("merged")) {
+        deferreds.push(addMerged(selected[n]));
+      }
+      else {
+        deferreds.push(addItem(selected[n]));
+      }
+    }
+  }
+  return deferreds
+}
+
+
+function invertInverted(){
+  var inverted = $(".invswitch:checked").map(function(){ return $(this).val(); }).get();
+  for (var n = 0; n < data.length; n++){
+    if (data[n].inverted & ($.inArray(data[n].slug, inverted) == -1)){
+      data[n].values = invertData(data[n].values);
+      data[n].inverted = false;
+    } else if ((data[n].inverted === undefined || !data[n].inverted) & $.inArray(data[n].slug, inverted) > -1 ){
+      data[n].values = invertData(data[n].values);
+      data[n].inverted = true;
+    }
+  }
+}
+
+function updateChart(){
+  var deferreds = updateData()
+  if (deferreds){
+    $.when.apply($, deferreds).then(function(){
+      invertInverted();
+      updateEfficiency();
+      refreshChart();
+      d3.selectAll(".subtype-pd").moveToBack();
+      d3.selectAll(".subtype-qe").moveToBack();
+      d3.selectAll(".subtype-eff").moveToFront();
+    });
+  } else {
+    updateEfficiency();
+  }
+
+}
+
+function updateEfficiency(){
+  if (!options.calcEff){
+    $("#efficiency-text").attr('placeholder', 'disabled')
+    return
+  }
+  if (!($("#fluor-select :selected").val())){
+    return
+  }
+  var a = calcExEmPaths();
+  removeSubtypes('eff')
+  var fluorslug = $("#fluor-select :selected").val();
+  if (a.emvalues && a.emvalues.length){
+    var emeff = efficiency(a.emvalues, {slug: fluorslug, type: 'em'});
+  }
+  if (a.exvalues && a.exvalues.length){
+    var exeff = efficiency(a.exvalues, {slug: fluorslug, type: 'ex'});
+  }
+  var info = '';
+  if (exeff){
+    //if (options.normMergedEx)
+      //exeff.values = normalizeSpectrum(exeff.values, options.normMergedScalar)[0]
+    pushData(exeff);
+    if (exeff.efficiency)
+      info += 'Ex: ' + String(Math.round(exeff.efficiency * 1000) / 10) + '%'
+  }
+  if (emeff){
+    pushData(emeff);
+    if (emeff.efficiency){
+      if (info)
+        info +=', '
+      info += 'Em: ' + String(Math.round(emeff.efficiency * 1000) / 10) + '%'
+    }
+  }
+
+  if (emeff != undefined & exeff !=undefined){
+    var b = exeff.efficiency * exeff.scalar * emeff.efficiency * emeff.scalar;
+    if (b)
+      info = String(Math.round(b/10) / 100) + ' (' + info + ')'
+  }
+
+  if (info != ''){
+    $("#efficiency-text").attr('placeholder', info)
+  }
+  else{
+    $("#efficiency-text").attr('placeholder', 'Select a fluor and config...')
+  }
+
+}
+
+
+$(function() {
+
+  $("#scale-camera").change(function(){
+    updateEfficiency();
+    refreshChart();
+  });
+
+
+  $(".invswitch, .filter-selector, .fluor-selector, #light-select, #camera-select, scaleToEC_input, scaleToQY_input, #merge-light-exfilter").change(function(){
+    if (options.oneAtaTime && (!$(this).hasClass('invswitch'))){
+      $(this).closest('.card').find('input').not(this).prop('checked', false)
+    }
+    updateChart();
+  });
+
+  $('#config-select').change(function(){
+
+    $('.filter-selector:checked').each(function(){
+        $(this).prop("checked", false )
+      });
+
+    var selected = $(this).find(":selected")
+    $.each(selected.data('filters'), function(i ,d){
+      $("#invswitch-filter-" + d[0]).prop("checked", d[2] );
+      $("#listfilter-" + d[0]).prop("checked", true );
+
+    })
+    var wave = selected.data('laser');
+    var light = selected.data('light');
+    var camera = selected.data('camera');
+    if (wave) {
+      $("#light-select option[value='laser-" + wave + "']").prop('selected', true)
+    }
+    else if (light) {
+      $("#light-select option[data-id='" + light + "']").prop('selected', true)
+    }
+    else {
+      //$("#light-select option[value='']").prop('selected', true)
+    }
+    if (camera) {
+      $("#camera-select option[data-id='" + camera + "']").prop('selected', true)
+    }
+    updateChart();
+  });
+
+  $(".switchmodal").click(function(e) {
+    e.preventDefault();
+    $("#settingsModal").modal('hide');
+    $("#embedModal").modal('show');
+  });
+
+});
+
+$(window).on('load', function() {
+  setTimeout(function(){
+      autoSizeText();
+      chart.update();
+      if ($(document).width() < 576){
+          chart.legend.maxKeyLength(15);
+      }
+  }, 100);
+});
+
+
+var topofDiv = $(".spectra-wrapper").offset().top;
+$(window).scroll(function(){
+    if($(window).scrollTop() > (topofDiv)){
+      $(".spectra-wrapper").css('box-shadow', '0 12px 8px -8px rgba(0,0,0,.2)');
+    }
+    else{
+      $(".spectra-wrapper").css('box-shadow', 'none');
+    }
+});
+
+
+$("#fluor-select").select2({ theme: "bootstrap", width: 100});
+
+
+$(function() {
+  for (var i=0; i < scopespectra.length; i++){
+    localData[scopespectra[i].slug] = [scopespectra[i]];
+  }
+});
+
+
