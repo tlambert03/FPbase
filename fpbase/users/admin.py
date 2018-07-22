0d4705bb8753f58ff5ff6a3e597943bb50668e2d
Talley Lambert <talley.lambert@gmail.com>
2018-07-22 12:41:25 -0400

    add collections microscopes and avatar to user in admin

--- fpbase/users/admin.py
+++ fpbase/users/admin.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.db.models import Count
 from .models import User
+from avatar.templatetags.avatar_tags import avatar_url
 
 
 class MyUserChangeForm(UserChangeForm):
@@ -34,11 +37,11 @@ class MyUserAdmin(AuthUserAdmin):
     form = MyUserChangeForm
     add_form = MyUserCreationForm
     fieldsets = (
-        ('User Profile', {'fields': ('name', 'email_verified')}),
+        ('User Profile', {'fields': ('avatar', 'name', 'email_verified', 'microscopes', 'collections')}),
     ) + AuthUserAdmin.fieldsets
     list_display = ('username', 'email', '_date_joined', '_last_login', '_logins', '_collections', 'social', )
     search_fields = ['name']
-    readonly_fields = ('email_verified', 'social')
+    readonly_fields = ('avatar', 'email_verified', 'social', 'microscopes', 'collections')
 
     def _date_joined(self, obj):
         return obj.date_joined.strftime("%Y/%m/%d")
@@ -50,9 +53,30 @@ class MyUserAdmin(AuthUserAdmin):
         return ''
     _last_login.admin_order_field = 'last_login'
 
+    def avatar(self, obj):
+        url = '<img src="{}" />'.format(avatar_url(obj))
+        return mark_safe(url)
+    avatar.allow_tags = True
+
     def email_verified(self, obj):
         return any([e.verified for e in obj.emailaddress_set.all()])
     email_verified.boolean = True
+
+    def microscopes(self, obj):
+        def _makelink(m):
+            url = reverse("admin:proteins_microscope_change", args=(m.pk,))
+            return '<a href="{}">{}</a>'.format(url, m.name)
+        links = [_makelink(m) for m in obj.microscopes.all()]
+        return mark_safe(", ".join(links))
+    microscopes.short_description = 'microscopes'
+
+    def collections(self, obj):
+        def _makelink(m):
+            url = reverse("proteins:collection-detail", args=(m.pk,))
+            return '<a href="{}">{}</a>'.format(url, m.name)
+        links = [_makelink(m) for m in obj.proteincollections.all()]
+        return mark_safe(", ".join(links))
+    collections.short_description = 'collections'
 
     def social(self, obj):
         return ", ".join([q.provider.title() for q in obj.socialaccount_set.all()])
