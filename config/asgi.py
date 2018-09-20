2ab0d75887347aa6b116fc62d989bdc327a3c586
Talley Lambert <talley.lambert@gmail.com>
2018-09-20 16:16:11 -0400

    remove sentry from asgi for now

--- config/asgi.py
+++ config/asgi.py
@@ -10,6 +10,6 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
 application = get_default_application()
-if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-    application = Sentry(application)
+# if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+#     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+#     application = Sentry(application)
