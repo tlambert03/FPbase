1ab92f74f9a5e0cfc1f1b1d3535d0e9f2931537b
tlambert03 <talley.lambert@gmail.com>
2018-10-07 12:01:35 -0400

    think i fixed the microscope form

--- proteins/forms/microscope.py
+++ proteins/forms/microscope.py
@@ -273,6 +273,7 @@ class OpticalConfigForm(forms.ModelForm):
         label="Excitation Filter(s)",
         queryset=Filter.objects.all(), required=False,
         widget=autocomplete.ModelSelect2Multiple(
+            url='proteins:filter-autocomplete',
             attrs={
                 'class': 'custom-select',
                 'data-theme': 'bootstrap',
@@ -285,7 +286,7 @@ class OpticalConfigForm(forms.ModelForm):
         label="Emission Filter(s)",
         queryset=Filter.objects.all(), required=False,
         widget=autocomplete.ModelSelect2Multiple(
-            # url='proteins:filter-autocomplete',
+            url='proteins:filter-autocomplete',
             attrs={
                 'data-theme': 'bootstrap',
                 'data-width': "100%",
@@ -297,6 +298,7 @@ class OpticalConfigForm(forms.ModelForm):
         label="Advanced: Reflective Emission Filter(s)",
         queryset=Filter.objects.all(), required=False,
         widget=autocomplete.ModelSelect2Multiple(
+            url='proteins:filter-autocomplete',
             attrs={
                 'data-theme': 'bootstrap',
                 'data-width': "100%",
@@ -309,6 +311,7 @@ class OpticalConfigForm(forms.ModelForm):
         label="Dichroic Filter(s)",
         queryset=Filter.objects.all(), required=False,
         widget=autocomplete.ModelSelect2Multiple(
+            url='proteins:filter-autocomplete',
             attrs={
                 'data-theme': 'bootstrap',
                 'data-width': "100%",
