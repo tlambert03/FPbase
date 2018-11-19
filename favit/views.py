a7b6fadfb37fb15325c0d61b59447dfbe21a9bc1
Talley Lambert <talley.lambert@gmail.com>
2018-11-19 15:58:57 -0500

    clear cache when page is favorited

--- favit/views.py
+++ favit/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
+from django.apps import apps
 from .models import Favorite
+from fpbase.util import uncache_protein_page
 
 
 @login_required
@@ -25,6 +27,15 @@ def add_or_remove(request):
         'status': status,
         'fav_count': Favorite.objects.for_object(obj_id, app_model).count()
     }
+
+    if (str(app_model) == 'proteins.Protein'):
+        try:
+            slug = apps.get_model(app_model).objects.get(id=obj_id).slug
+            uncache_protein_page(slug, request)
+        except Exception as e:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.error('failed to uncache protein: {}'.format(e))
 
     return JsonResponse(response)
 
