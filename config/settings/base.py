83b85c4d49fd6b682b21350254823136d76ec316
Talley Lambert <talley.lambert@gmail.com>
2018-11-20 19:50:42 -0500

    webpack whitenoise 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -214,7 +214,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(ROOT_DIR.path('static/dist')),
+    str(ROOT_DIR.path('static/')),
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
