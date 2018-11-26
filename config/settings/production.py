7b5c5171761300b11dda76dc9da1485c8cd6cf20
Talley Lambert <talley.lambert@gmail.com>
2018-11-26 10:53:33 -0500

    add back django storages as default_file_storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -111,7 +111,7 @@ AWS_HEADERS = {
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
 MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = None
 AWS_IS_GZIPPED = True
 
