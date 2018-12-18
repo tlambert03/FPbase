115f96d83cf92c1953cd455c60e6e11c20d5e549
Talley Lambert <talley.lambert@gmail.com>
2018-12-18 11:10:22 -0500

    fix author 404 bug

--- references/urls.py
+++ references/urls.py
@@ -5,8 +5,8 @@ app_name = 'references'
 
 urlpatterns = [
     url(r'^autocomplete/$', ReferenceAutocomplete.as_view(create_field='doi'), name='reference-autocomplete',),
-    url(r'^(?P<pk>[-\w\/\.]+)/$', ReferenceDetailView.as_view(), name='reference-detail'),
     url(r'^author/(?P<pk>[-\w]+)/$', AuthorDetailView.as_view(), name='author-detail'),
     url(r'^ajax/add_excerpt/(?P<pk>[-\w]+)$', add_excerpt, name='add_excerpt'),
 
+    url(r'^(?P<pk>[-\w\/\.]+)/$', ReferenceDetailView.as_view(), name='reference-detail'),
 ]
