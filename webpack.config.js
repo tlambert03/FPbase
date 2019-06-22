9eec5aea6e7474d00c75f21bd7f808669eaae22a
Talley Lambert <talley.lambert@gmail.com>
2019-06-22 14:19:42 -0400

    heavily modified webpack config, chunking

--- webpack.config.js
+++ webpack.config.js
@@ -1,182 +1,191 @@
-const path = require('path');
-const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
+const path = require("path")
+const webpack = require("webpack")
+const autoprefixer = require("autoprefixer")
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const BundleTracker = require("webpack-bundle-tracker")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default
+const ImageminWebP = require("imagemin-webp")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const SentryCliPlugin = require("@sentry/webpack-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const CSSnano = require("cssnano")
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const BundleTracker = require('webpack-bundle-tracker');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
-const ImageminWebP = require("imagemin-webp");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const SentryCliPlugin = require('@sentry/webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-//const { VueLoaderPlugin } = require('vue-loader');
-
-const devMode = process.env.NODE_ENV !== 'production';
-const hotReload = process.env.HOT_RELOAD === '1';
-
-//const vueRule = {
-//  test: /\.vue$/,
-//  use: 'vue-loader',
-//  exclude: /node_modules/
-//};
+const devMode = process.env.NODE_ENV !== "production"
+const hotReload = process.env.HOT_RELOAD === "1"
 
 const styleRule = {
   test: /\.(sa|sc|c)ss$/,
   use: [
     MiniCssExtractPlugin.loader,
     {
-      loader: 'css-loader',
+      loader: "css-loader",
       options: {
         sourceMap: true
       }
     },
     {
-      loader: 'postcss-loader',
+      loader: "postcss-loader",
       options: {
         plugins: () => [
           autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: ["last 2 versions"]
           }),
-          require('cssnano')
-        ],
-      },
+          CSSnano
+        ]
+      }
     },
-    'sass-loader',
+    "sass-loader"
   ]
-};
+}
 
 const jsRule = {
   test: /\.jsx?$/,
-  exclude: /node_modules\/(?!smiles-drawer\/).*/,
+  exclude: /node_modules\/(?![qs|debounce\-raf|smiles\-drawer]).*/,
   use: {
-    loader: 'babel-loader',
+    loader: "babel-loader",
     options: {
       presets: [
-        ["@babel/preset-env", {
-          useBuiltIns: "entry",
-          corejs: 3,
-        }],
-        '@babel/preset-react'
+        [
+          "@babel/preset-env",
+          {
+            useBuiltIns: "entry",
+            corejs: 3
+          }
+        ],
+        "@babel/preset-react"
       ],
-      plugins: ["@babel/plugin-syntax-dynamic-import"],
+      plugins: ["@babel/plugin-syntax-dynamic-import"]
     }
   }
-};
+}
 
 const assetRule = {
   test: /.(jpg|png|woff(2)?|eot|ttf|svg)$/,
-  loader: 'file-loader'
-};
+  loader: "file-loader"
+}
 
 const plugins = [
   new webpack.ProgressPlugin(),
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery'
+    $: "jquery",
+    jQuery: "jquery"
   }),
   new webpack.IgnorePlugin(/vertx/),
   new BundleTracker({
-    filename: './webpack-stats.json'
+    filename: "./webpack-stats.json"
   }),
-  //new VueLoaderPlugin(),
   new MiniCssExtractPlugin({
-    filename: '[name].css',
-    chunkFilename: '[id].css'
+    filename: "[name].[contenthash].css",
+    chunkFilename: "[id].[chunkhash].css"
   }),
   new BundleAnalyzerPlugin({
-    analyzerMode: 'static',
+    analyzerMode: "static",
     openAnalyzer: false
   }),
-  new webpack.HotModuleReplacementPlugin(),
   new CleanWebpackPlugin(),
-  new CopyWebpackPlugin([{
-    from: './static/src/images/**/*',
-    to: path.resolve('./static/dist/images/[name].webp'),
-    toType: 'template',
-  }, ]),
-  new CopyWebpackPlugin([{
-    from: './static/src/js/sentry.*.js',
-    to: path.resolve('./static/dist/sentry.js'),
-  }, ]),
+  new CopyWebpackPlugin([
+    {
+      from: "./static/src/images/**/*",
+      to: path.resolve("./static/dist/images/[name].webp"),
+      toType: "template"
+    }
+  ]),
+  new CopyWebpackPlugin([
+    {
+      from: "./static/src/js/sentry.*.js",
+      to: path.resolve("./static/dist/sentry.js")
+    }
+  ]),
   new ImageminWebpackPlugin({
     test: /\.(webp)$/i,
     plugins: [
       ImageminWebP({
         quality: 90,
-        sharpness: 1,
-      }),
-    ],
+        sharpness: 1
+      })
+    ]
   }),
-  new CopyWebpackPlugin([{
-    from: './static/src/images/**/*',
-    to: path.resolve('./static/dist/images/[name].[ext]'),
-    toType: 'template',
-  }, ]),
-];
+  new CopyWebpackPlugin([
+    {
+      from: "./static/src/images/**/*",
+      to: path.resolve("./static/dist/images/[name].[ext]"),
+      toType: "template"
+    }
+  ])
+]
 
 if (devMode) {
-  styleRule.use = ['css-hot-loader', ...styleRule.use];
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+if (devMode) {
+  styleRule.use = ["css-hot-loader", ...styleRule.use]
 } else {
   plugins.push(
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
+      NODE_ENV: "development",
       SOURCE_VERSION: false,
       SENTRY_DSN: false,
-      SENTRY_AUTH_TOKEN: false,
+      SENTRY_AUTH_TOKEN: false
     })
-  );
+  )
 
   if (process.env.SENTRY_AUTH_TOKEN && !process.env.CI) {
     plugins.push(
       new SentryCliPlugin({
-        include: 'static/',
+        include: "static/",
         release: process.env.SOURCE_VERSION,
-        ignore: ['node_modules', 'webpack.config.js', 'static/src/js/pdb/LiteMol-plugin.js'],
+        ignore: [
+          "node_modules",
+          "webpack.config.js",
+          "static/src/js/pdb/LiteMol-plugin.js"
+        ]
       })
-    );
+    )
   }
 }
 
 module.exports = {
   context: __dirname,
   entry: {
-    main: './static/src/index.js',
-    embedscope: './static/src/embedscope.js',
-    litemol: './static/src/my-litemol.js'
+    main: "./static/src/index.js",
+    embedscope: "./static/src/embedscope.js",
+    litemol: "./static/src/my-litemol.js",
+    spectraViewer: "./static/src/spectra-viewer.js",
+    blast: "./static/src/blast-app.js"
   },
   output: {
-    path: path.resolve('./static/dist/'),
-    filename: '[name].js',
-    publicPath: hotReload ? 'http://localhost:8080/static/' : '/static/',
-    //publicPath: hotReload ? 'http://10.0.2.2:8080/static/' : '/static/',
-    chunkFilename: '[name]-bundle.js',
+    path: path.resolve("./static/dist/"),
+    filename: "[name].[hash].js",
+    publicPath: hotReload ? "http://localhost:8080/static/" : "/static/",
+    // publicPath: hotReload ? 'http://10.0.2.2:8080/static/' : '/static/',
+    chunkFilename: "[name].[chunkhash].js"
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
     alias: {
       jquery: "jquery/src/jquery"
     }
   },
-  stats: 'minimal',
-  devtool: devMode ? 'cheap-eval-source-map' : 'source-map',
+  devtool: devMode ? "cheap-eval-source-map" : "source-map",
   devServer: {
     hot: true,
     quiet: false,
     port: 8080,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*"
     }
   },
   module: {
     rules: [jsRule, styleRule, assetRule]
   },
   externals: {
-    Sentry: 'Sentry'
+    Sentry: "Sentry"
   },
-  plugins: plugins,
+  plugins,
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -190,10 +199,10 @@ module.exports = {
       cacheGroups: {
         commons: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'initial',
-        },
+          name: "vendor",
+          chunks: "initial"
+        }
       }
     }
-  },
-};
+  }
+}
