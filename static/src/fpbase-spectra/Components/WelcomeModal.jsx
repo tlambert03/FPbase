c6ed7b3f87681b86d171e6424a729fdca012d709
Talley Lambert <talley.lambert@gmail.com>
2019-06-24 21:34:07 -0400

    minor

--- static/src/fpbase-spectra/Components/WelcomeModal.jsx
+++ static/src/fpbase-spectra/Components/WelcomeModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 import Checkbox from "@material-ui/core/Checkbox"
