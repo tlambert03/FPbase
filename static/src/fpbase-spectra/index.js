09c3d9d62839b6df28e58795afdebbaba1f9bb1c
Talley Lambert <talley.lambert@gmail.com>
2019-06-29 07:57:48 -0400

    add why-did-you-render

--- static/src/fpbase-spectra/index.js
+++ static/src/fpbase-spectra/index.js
@@ -2,7 +2,13 @@ import React, { useRef } from "react"
 import "./index.css"
 import App from "./App"
 import { ApolloProvider } from "react-apollo-hooks"
+//import { ApolloProvider } from "@apollo/react-hooks"
 import initializeClient from "./client/client"
+
+if (process.env.NODE_ENV !== "production") {
+  const whyDidYouRender = require("@welldone-software/why-did-you-render")
+  whyDidYouRender(React, { include: [/App.+/], logOnDifferentValues: true })
+}
 
 const AppWrapper = ({ uri }) => {
   const client = useRef(initializeClient({ uri }))
