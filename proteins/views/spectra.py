1cc1cc77adf0f60842c9c9922779357c2624dad7
Talley Lambert <talley.lambert@gmail.com>
2019-07-06 06:41:53 -0400

    remove GET method requirement from FRET page

--- proteins/views/spectra.py
+++ proteins/views/spectra.py
@@ -50,38 +50,35 @@ def fret_chart(request):
     """ renders html for protein spectra page  """
     template = "fret.html"
 
-    if request.method == "GET":
+    if request.is_ajax():
+        L = cache.get("forster_list")
+        if not L:
+            job = cache.get("calc_fret_job")
+            if cache.get("calc_fret_job"):
+                result = app.AsyncResult(job)
+                if result.ready():
+                    L = result.get()
+                    cache.set("forster_list", L, 60 * 60 * 24)
+                    cache.delete("calc_fret_job")
+            else:
+                job = calc_fret.delay()
+                if job:
+                    cache.set("calc_fret_job", job.id)
+        return JsonResponse({"data": L})
 
-        if request.is_ajax():
-            L = cache.get("forster_list")
-            if not L:
-                job = cache.get("calc_fret_job")
-                if cache.get("calc_fret_job"):
-                    result = app.AsyncResult(job)
-                    if result.ready():
-                        L = result.get()
-                        cache.set("forster_list", L, 60 * 60 * 24)
-                        cache.delete("calc_fret_job")
-                else:
-                    job = calc_fret.delay()
-                    if job:
-                        cache.set("calc_fret_job", job.id)
-            return JsonResponse({"data": L})
-
-        else:
-            slugs = State.objects.filter(spectra__subtype__in=("ex", "ab")).values(
-                "slug", "protein__name", "spectra__category", "spectra__subtype"
-            )
-            slugs = [
-                {
-                    "slug": x["slug"],
-                    "category": x["spectra__category"],
-                    "subtype": x["spectra__subtype"],
-                    "name": x["protein__name"],
-                }
-                for x in slugs
-            ]
-            return render(request, template, {"probeslugs": slugs})
+    slugs = State.objects.filter(spectra__subtype__in=("ex", "ab")).values(
+        "slug", "protein__name", "spectra__category", "spectra__subtype"
+    )
+    slugs = [
+        {
+            "slug": x["slug"],
+            "category": x["spectra__category"],
+            "subtype": x["spectra__subtype"],
+            "name": x["protein__name"],
+        }
+        for x in slugs
+    ]
+    return render(request, template, {"probeslugs": slugs})
 
 
 class SpectrumCreateView(CreateView):
