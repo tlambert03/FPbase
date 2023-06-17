8ebd2a37161da75ca0ad0c80cc5bc93bf4dc1c21
Talley Lambert <talley.lambert@gmail.com>
2023-06-17 15:57:57 -0400

    fix: fix embedscope

--- frontend/src/embedscope.js
+++ frontend/src/embedscope.js
@@ -1,6 +1,7 @@
 import 'jquery';
 import 'bootstrap';
 import 'nouislider';
+import d3 from "d3";
 import './js/nv.d3.js';
 import 'select2/dist/js/select2.full.js';
 import './js/my-fontawesome.js';
