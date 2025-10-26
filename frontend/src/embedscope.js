86f779bdc634c5bd531aa9fcf957d80c2e655746
Talley Lambert <talley.lambert@gmail.com>
2025-10-26 14:18:47 -0400

    fix embedscope js

--- frontend/src/embedscope.js
+++ frontend/src/embedscope.js
@@ -4,7 +4,8 @@ import "./js/sentry-init.js"
 import "jquery"
 import "bootstrap"
 import "nouislider"
-import * as d3 from "d3"
+// D3 v3 is loaded from CDN for microscope.js compatibility (see microscope_embed.html)
+// Do NOT import D3 v7 here as it will overwrite the global d3 object and break nvd3
 import "select2/dist/js/select2.full.js"
 // microscope.js loaded separately via CDN on microscope pages
 
