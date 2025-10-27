ddc4e71b274a93d01155d4333b097aea6529eb7b
Talley Lambert <talley.lambert@gmail.com>
2025-10-27 09:37:17 -0400

    Misc fixes from sentry (#308)
    fixes FPBASE-5HP
    fixes FPBASE-5JT
    fixes FPBASE-5H3
    Fixes FPBASE-5KP
    fixes FPBASE-5KZ
    
    * Fix React SpectraViewer iteration errors with non-array exNorm Values
    
    * update replayIntegration
    
    * fix duplicate dropdown in spectrum url builder
    
    * remove apollog devtools
    
    * remove twitter from spectra share button
    
    * fix pdf export

--- frontend/src/js/project.js
+++ frontend/src/js/project.js
@@ -204,10 +204,12 @@ $(function() {
 
 /////////////////. Spectra Image URL Builder
 
-$("#proteinSlug").select2({
-  theme: "bootstrap",
-  width: "80%",
-  ajax: {
+// Only initialize Select2 if not already initialized (fixes duplicate select boxes)
+if (!$("#proteinSlug").hasClass("select2-hidden-accessible")) {
+  $("#proteinSlug").select2({
+    theme: "bootstrap",
+    width: "80%",
+    ajax: {
     theme: "bootstrap",
     containerCssClass: ":all:",
     width: "auto",
@@ -223,9 +225,38 @@ $("#proteinSlug").select2({
         _type: params._type
       }
       return query
+    },
+    processResults: function(data) {
+      // Ensure all results have an id property (fixes FPBASE-5H3)
+      // The autocomplete endpoint returns results, but we need to ensure
+      // each item has an id that can be safely converted to string
+      if (data.results) {
+        data.results = data.results.map(function(item) {
+          // If item doesn't have an id, use text as fallback
+          if (!item.id && item.text) {
+            item.id = item.text
+          }
+          return item
+        })
+      }
+      return data
     }
+  },
+  // Safely handle cases where id might still be undefined
+  templateResult: function(item) {
+    if (!item.id) {
+      return item.text
+    }
+    return item.text || item.id
+  },
+  templateSelection: function(item) {
+    if (!item.id) {
+      return item.text
+    }
+    return item.text || item.id
   }
-})
+  })
+}
 
 function buildURL() {
   var ext = "." + $("#fileTypeSelect").val()
--- packages/spectra/src/Components/ShareButton.jsx
+++ packages/spectra/src/Components/ShareButton.jsx
@@ -33,23 +33,6 @@ import {
   GET_EX_NORM,
 } from "../client/queries"
 
-window.twttr = (function(d, s, id) {
-  const fjs = d.getElementsByTagName(s)[0]
-  const t = window.twttr || {}
-  if (d.getElementById(id)) return t
-  const js = d.createElement(s)
-  js.id = id
-  js.src = "https://platform.twitter.com/widgets.js"
-  fjs.parentNode.insertBefore(js, fjs)
-
-  t._e = []
-  t.ready = function(f) {
-    t._e.push(f)
-  }
-
-  return t
-})(document, "script", "twitter-wjs")
-
 const useStyles = makeStyles(theme => ({
   textField: {
     flexBasis: 200,
