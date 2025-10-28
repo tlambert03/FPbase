a0187e65e466f657932e2a21d0dc9479f76705eb
Talley Lambert <talley.lambert@gmail.com>
2025-10-28 17:22:48 -0400

    ignore litmol stuff, remove old e2e

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests/e2e',
@@ -25,4 +25,4 @@ export default defineConfig({
     reuseExistingServer: true,
     timeout: 5000,
   },
-});
+})
