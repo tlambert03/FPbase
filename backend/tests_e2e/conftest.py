ba87f38a7d8e70cc8e9a895f656ab60b25cc174a
Talley Lambert <talley.lambert@gmail.com>
2025-10-28 13:48:44 -0400

    bump ci timeout

--- backend/tests_e2e/conftest.py
+++ backend/tests_e2e/conftest.py
@@ -47,7 +47,8 @@ if TYPE_CHECKING:
     from django.contrib.auth.models import AbstractUser
     from playwright.sync_api import Browser, BrowserContext, ConsoleMessage, Page, ViewportSize
 
-DEFAULT_TIMEOUT = int(os.getenv("DEFAULT_TIMEOUT", 8000))  # milliseconds
+CI = bool(os.getenv("CI"))
+DEFAULT_TIMEOUT = int(os.getenv("DEFAULT_TIMEOUT", 8000 if CI else 4000))  # ms
 VIEWPORT_SIZE: ViewportSize = {"width": 1020, "height": 1200}
 
 # Required for pytest-playwright fixtures to work with Django ORM in test setup.
