1d00d21c713a34cd889f49a082caed3d401051f4
Talley Lambert <talley.lambert@gmail.com>
2025-10-31 22:02:31 -0400

    just comments

--- packages/blast/src/ReportView.jsx
+++ packages/blast/src/ReportView.jsx
@@ -8,7 +8,6 @@ import Tabs from "@mui/material/Tabs"
 import { makeStyles } from "@mui/styles"
 import React, { useEffect, useState } from "react"
 
-// Use window.jQuery from CDN instead of importing (importmap doesn't work in Vite dev mode)
 const $ = window.jQuery
 
 import BlastReportAlignments from "./ReportAlignments.jsx"
