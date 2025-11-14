b671324e24b222b6a978cd9bb400ff2a120799ae
Talley Lambert <talley.lambert@gmail.com>
2025-11-14 08:53:44 -0500

    warm caches

--- backend/fpbase/apps.py
+++ backend/fpbase/apps.py
@@ -12,5 +12,11 @@ class FPbaseConfig(AppConfig):
     def ready(self):
         """Import signal handlers when Django starts."""
         from fpbase.cache_utils import _register_signal_handlers
+        from proteins.models.microscope import get_cached_optical_configs
+        from proteins.models.spectrum import get_cached_spectra_info
 
         _register_signal_handlers()
+
+        # warm caches
+        get_cached_spectra_info()
+        get_cached_optical_configs()
